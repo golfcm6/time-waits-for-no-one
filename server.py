7b6de2eb2f7145765b36f0caf5f4aa50c80ddb6f
Ash Ahmed <ashlanahmed@college.harvard.edu>
2023-03-06 15:40:49 -0800

    more scaffolding

--- server.py
+++ server.py
@@ -4,8 +4,8 @@ import threading
 import sys
 import random
 
-class server:
-    def __init__(self, host1, port1, host2, port2, host3, port3, log_name):
+class Server:
+    def __init__(self, host1, port1, host2, port2, host3, port3, log_name, name):
         # host1 and port1 are for this server
         self.host1 = host1
         self.port1 = port1
@@ -14,6 +14,14 @@ class server:
         self.host3 = host3
         self.port3 = port3
         self.log_name = log_name
+        # our convention depends on knowing if this is server A, B, or C:
+        # - if A, you create socket to B and C (bind call + listen)
+        # - if B, you create socket to C and connect to A
+        # - if C, you just connect to existing A and B sockets
+
+        # A must be made before B, B must be made before C
+        # DO THE LOGIC HERE
+        self.name = name
 
         # used whenever a data structure gets updated
         self.ds_lock = threading.Lock()
@@ -23,40 +31,32 @@ class server:
         # initialize the logic clock to be at time 0
         self.clock = 0
 
+        self.socket_a = 0
+        self.socket_b = 0
+        self.socket_c = 0
+	
         # START 3 THREADS HERE: ONE FOR LISTENING TO HOST2/PORT2, ONE FOR LISTENING TO HOST3/PORT3, AND ONE PROCESS THREAD
 
-    def listening(self, host, port):
+    def listening(self, s):
         # logic for the threads that listen to other servers and updates network queue
-        pass
+        # this depends on if self.name is A, B, or C (you either bind or connect)
+	    while True:
+			c, addr = s.accept()
+			print('Connected to :', addr[0], ':', addr[1])
     
     def process(self):
         # all logic for reading queue/doing shit
         pass
+    
+
 
 def main():
 	args = sys.argv[1:]
-	assert len(sys.argv) == 2, f"provide host address"
+	assert len(sys.argv) == 8, f"provide h1, p1, h2, p2, h3, p3, file_name, name"
 	
-	host = args[0]
-	port = 49153
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind((host, port))
-	print("socket binded to port", port)
-
-	# put the socket into listening mode - we accept everything, so no need to specify a backlog
-	s.listen()
-	print("socket is listening")
-
-	# a forever loop until client wants to exit
-	while True:
-		# establish connection with client
-		c, addr = s.accept()
-
-		print('Connected to :', addr[0], ':', addr[1])
-
-		# Start a new thread
-		start_new_thread(threaded, (c,))
+	me = Server(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7])
 
 
 if __name__ == '__main__':
 	main()
+	
